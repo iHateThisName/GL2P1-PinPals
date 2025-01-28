f6d4ef382d7f52b9fa183e09d8f6c3b42f24233e
Einar <einarsterten@gmail.com>
2025-01-28 22:55:11 +0100

    Testing layers, tweaking bumpers

---
+++ Assets/_Scripts/EnumLayers.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public enum EnumLayers
+{
+        Player01 = 1,
+        Player02 = 2,
+        Player03 = 3,
+        Player04 = 4,
+        Player05 = 5,
+        Player06 = 6,
+        Player07 = 7,
+        Player08 = 8,
+        Player09 = 9,
+        Player10 = 10,
+        Player11 = 11,
+        Player12 = 12,
+        Player13 = 13,
+        Player14 = 14,
+        Player15 = 15,
+        Player16 = 16
+}
