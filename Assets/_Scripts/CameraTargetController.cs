bbb12b952dd0b26315b07c4f735e0a824efa12c5
iHateThisName <ivarravi0@gmail.com>
2025-01-29 14:11:09 +0100

    Make camera follow player’s height relative to the ground
    Works with local multiplayer

--- Assets/_Scripts/CameraTargetController.cs
+++ Assets/_Scripts/CameraTargetController.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 // Ivar
 public class CameraTargetController : MonoBehaviour {
-    private Transform _playerPinBall;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _distanceFromGround = 30f;
+
+    [SerializeField] private Transform _playerPinBall;
+    private Transform _ground;
 
     private void Start() {
-        this._playerPinBall = this.transform.parent.transform;
+        SetUp();
+        PositionCamera();
     }
+
+    private void SetUp() {
+        this._ground = GameObject.FindGameObjectWithTag("Ground").transform;
+    }
+
+    private void PositionCamera() {
+        //// Move the camera back along the ground's forward vector
+        //_camera.transform.position = _ground.position - _ground.forward * _distanceFromGround;
+        //// Ensure the camera looks at the ground
+        //_camera.transform.LookAt(_ground.position);
+
+        if (this._camera == null) return;
+        // Calculate the camera position
+        Vector3 cameraFollowPosition = _ground.position;
+        cameraFollowPosition.y = this.gameObject.transform.position.y; // Follow player's height, this will make the camera feel like its zooming out when the player is moving up
+
+        Vector3 cameraOffset = -_ground.forward * _distanceFromGround;
+        _camera.transform.position = cameraFollowPosition + cameraOffset;
+    }
+
     private void LateUpdate() {
-        if (this._playerPinBall != null) {
-            this.transform.position = this._playerPinBall.position; // Copy the position
-            this.transform.rotation = Quaternion.identity; // Dont rotate
-        }
+        if (this._playerPinBall == null) return;
+        this.transform.position = this._playerPinBall.position + this.gameObject.transform.parent.position; // Copy the position
+        this.transform.rotation = Quaternion.identity; // Dont rotate
+
+        PositionCamera();
     }
 }
