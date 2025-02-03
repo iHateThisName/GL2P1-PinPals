01a7c217d96c03a8e640b86e9b9b2eeb863a424b
iHateThisName <ivarravi0@gmail.com>
2025-02-03 18:31:29 +0100

    Camera, Offset the camera more when the ball is falling.
    Tweaking values, play testing

--- Assets/_Scripts/CameraTargetController.cs
+++ Assets/_Scripts/CameraTargetController.cs
@@ -6,17 +6,24 @@ public class CameraTargetController : MonoBehaviour {
 
     [Header("Camera Configuration")]
     [SerializeField] private Camera _camera;
-    [SerializeField] private float _distanceFromGround = 30f;
-    [SerializeField] private float _zPosOffset = 10f;
-    [SerializeField] private float _zPosOffsetFalling = -10f;
+    [SerializeField] private float _distanceFromGround = 50f;
+    [SerializeField] private float _zPosOffset = -10f;
+    [SerializeField] private float _zPosOffsetFalling = -25f;
     [SerializeField] private float _cameraBoundingXOffset = 43.5f;
     [SerializeField] private float _cameraBoundingZMinOffset = 15.5f;
-    [SerializeField] private float _xLerpSpeed = 5f;
+    [Tooltip("Speed the camera's x targetPosition moves towards the target targetPosition. Higher value = faster.")]
+    [SerializeField] private float _xLerpSpeed = 1f;
+    [Tooltip("Speed the camera's z targetPosition moves towards the target targetPosition. Higher value = faster.")]
     [SerializeField] private float _zLerpSpeed = 3f;
 
     private Transform _ground;
     private Vector3 _groundMin; // Bootom left
     private Vector3 _groundMax; // Top right
+
+    private bool _isFalling = false;
+    private float _previousZPosition;
+    private float _currentZPosOffset;
+
 
     private void Start() {
         SetUp();
@@ -26,6 +33,8 @@ public class CameraTargetController : MonoBehaviour {
 
     private void SetUp() {
         this._ground = GameObject.FindGameObjectWithTag("Ground")?.transform;
+        this._currentZPosOffset = this._zPosOffset;
+        this._previousZPosition = this.gameObject.transform.position.z;
         if (_ground == null) Debug.LogError("Ground not found in the scene. Please add a GameObject with the tag 'Ground'");
     }
 
@@ -40,12 +49,24 @@ public class CameraTargetController : MonoBehaviour {
 
     private void PositionCamera() {
         if (this._camera == null) return;
+        float deltaTime = Time.deltaTime;
+        Vector3 targetPosition = this.gameObject.transform.position;
+        Vector3 cameraPosition = this._camera.transform.position;
 
+        // Calculate what Offset to use, lerping between the two values
+        if (_isFalling) {
+            this._currentZPosOffset = Mathf.Lerp(this._currentZPosOffset, this._zPosOffsetFalling, deltaTime * this._zLerpSpeed);
+        } else {
+            this._currentZPosOffset = Mathf.Lerp(this._currentZPosOffset, this._zPosOffset, deltaTime * (this._zLerpSpeed * 1.2f));
+        }
 
-        Vector3 rawDistance = new Vector3(this.gameObject.transform.position.x, _distanceFromGround, this.gameObject.transform.position.z + this._zPosOffset);
+        // Get the raw targetPosition of the camera for following the player
+        float x = Mathf.Lerp(cameraPosition.x, targetPosition.x, deltaTime * this._xLerpSpeed);
+        float z = Mathf.Lerp(cameraPosition.z, targetPosition.z + this._currentZPosOffset, deltaTime * this._zLerpSpeed);
+        Vector3 rawDistance = new Vector3(x, this._distanceFromGround, z);
 
         // use clamp to ensure the camera is within the ground area
-        _camera.transform.position = new Vector3(
+        this._camera.transform.position = new Vector3(
             Mathf.Clamp(rawDistance.x, _groundMin.x + _cameraBoundingXOffset, _groundMax.x - _cameraBoundingXOffset),
             rawDistance.y,
             Mathf.Clamp(rawDistance.z, _groundMin.z + _cameraBoundingZMinOffset, _groundMax.z - _cameraBoundingXOffset)
@@ -53,12 +74,16 @@ public class CameraTargetController : MonoBehaviour {
 
     }
 
-    private void FixedUpdate() {
+    private void LateUpdate() {
         if (this._playerPinBall == null) return;
 
-        //this.transform.position = this._playerPinBall.position + this.gameObject.transform.parent.position;
+        //this.transform.targetPosition = this._playerPinBall.targetPosition + this.gameObject.transform.parent.targetPosition;
         this.transform.position = this._playerPinBall.position;
         this.transform.rotation = Quaternion.identity; // Dont rotate
+
+        // Detect if the gameObject is falling in the z direction with a minimum speed
+        _isFalling = this.gameObject.transform.position.z < _previousZPosition - 0.2f;
+        _previousZPosition = this.gameObject.transform.position.z;
 
         PositionCamera();
     }
