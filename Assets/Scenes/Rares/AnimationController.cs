76bc01d729acae94176baaba2bfc214e7eedbed8
Rayas <irasiserar@gmail.com>
2025-02-07 13:22:30 +0100

    AnimationForPlayerJoin

--- Assets/Scenes/Rares/AnimationController.cs
+++ Assets/Scenes/Rares/AnimationController.cs
@@ -6,14 +6,29 @@ public class TAnimationController : MonoBehaviour
 
     void Start()
     {
-        animator = GetComponent<Animator>(); // Get the Animator component
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) // When space is pressed
+        if (Input.GetKeyDown(KeyCode.A)) // Change the condition
         {
-            animator.SetTrigger("PlayAnimation"); // Set the trigger
+            animator.SetTrigger("PlayAnimationPlayer1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) // Change the condition
+        {
+            animator.SetTrigger("PlayAnimationPlayer2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) // Change the condition
+        {
+            animator.SetTrigger("PlayAnimationPlayer3");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F)) // Change the condition
+        {
+            animator.SetTrigger("PlayAnimationPlayer4");
         }
     }
 }
