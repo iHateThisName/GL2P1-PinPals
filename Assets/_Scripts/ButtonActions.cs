06134a04ce0519dd527c9c5951a6496477dc4188
iHateThisName <ivabr@stud.ntnu.no>
2025-03-21 11:12:52 +0100

    Controller Support for the pause menu.

--- Assets/_Scripts/ButtonActions.cs
+++ Assets/_Scripts/ButtonActions.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ButtonActions : MonoBehaviour {
+    [Header("Setup")]
+    [SerializeField] private EventSystem _eventSystem;
+    [SerializeField] private Selectable _elementToSelect;
 
+    void Start() {
+        _eventSystem = GameObject.FindAnyObjectByType<EventSystem>();
+        JumpToElement();
+    }
     public void OnResumeClick() => GameManager.Instance.OnPauseAction();
     public void OnRestartClick() => GameManager.Instance.ReloadGame();
     public void OnMainMenuClick() => GameManager.Instance.MainMenu();
+
+    public void JumpToElement() {
+        if (this._eventSystem == null)
+            Debug.Log("Missing system referenced.", this);
+
+        if (this._elementToSelect == null)
+            Debug.Log("Missing UI element reference.", this);
+
+        this._eventSystem.SetSelectedGameObject(this._elementToSelect.gameObject);
+    }
 }
