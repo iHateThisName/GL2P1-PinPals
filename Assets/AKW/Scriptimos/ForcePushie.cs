5c91d5811eec2310204abe7f1204bd7c20ab4fd7
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2025-04-05 17:00:01 +0200

    Pipes go Swooosh
    (Mario pipe Sound)

--- Assets/AKW/Scriptimos/ForcePushie.cs
+++ Assets/AKW/Scriptimos/ForcePushie.cs
@@ -5,14 +5,13 @@ public class ForcePushie : MonoBehaviour
     public Transform target;
     public float forceStrength = 2f;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         Rigidbody rb = other.attachedRigidbody;
         if (rb != null)
         {
             Vector3 forceDirection = (target.position - other.transform.position).normalized;
-
-            rb.AddForce(forceDirection * forceStrength, ForceMode.Impulse);
+            rb.AddForce(forceDirection * forceStrength, ForceMode.Force);
         }
     }
 }
