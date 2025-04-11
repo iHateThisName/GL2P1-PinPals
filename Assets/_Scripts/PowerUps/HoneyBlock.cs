e18337fb4817bec21b933fecaff4dcee00351adf
PythonXYZ <hilmirsg@gmail.com>
2025-04-11 14:32:40 +0200

    Honey Block Change
    Changed the linear damping so the player properly slows down now.

--- Assets/_Scripts/PowerUps/HoneyBlock.cs
+++ Assets/_Scripts/PowerUps/HoneyBlock.cs
@@ -1,33 +1,43 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class HoneyBlock : MonoBehaviour {
     private GameObject _honeyOwner;
+    private float _waitTime = 5f;
     public void AssignOwner(GameObject owner) {
         this._honeyOwner = owner;
     }
     private void Start() {
         this.gameObject.SetActive(true);
     }
-    //public async void OnTriggerEnter(Collider player) {
-    //    if (player.gameObject.name.Contains("Clone")) {
-    //        player.GetComponent<Rigidbody>().linearDamping = 5.0f;
-    //    }
-    //    // Detect every player in the collider then respawn them
-    //    if (player.gameObject != _honeyOwner) {
-    //        if (player.gameObject.tag.StartsWith("Player")) {
-    //            EnumPlayerTag tag = player.gameObject.GetComponent<PlayerReferences>().GetPlayerTag();
-    //            player.GetComponent<Rigidbody>().linearDamping = 5.0f;
-    //        }
-    //    }
-    //    if (player.gameObject.tag == ("Bumper")) {
-    //        Destroy(player.gameObject);
-    //    }
-    //    await Task.Delay(3000);
-    //    Destroy(this.gameObject);
-    //}
+    public void OnCollisionEnter(Collision player) {
 
-    public void OnTriggerExit(Collider player) {
-        player.GetComponent<Rigidbody>().angularDamping = 0f;
-        player.GetComponent<Rigidbody>().linearDamping = 0f;
+        if (player.gameObject.name.Contains("Clone"))
+        {
+            player.gameObject.GetComponent<Rigidbody>().linearDamping = 10.0f;
+        }
+        // Detects the players and de-accelerates their speed.
+        if (player.gameObject != _honeyOwner)
+        {
+            if (player.gameObject.tag.StartsWith("Player"))
+            {
+                PlayerReferences playerRef = player.gameObject.GetComponent<PlayerReferences>();
+                EnumPlayerTag tag = playerRef.GetPlayerTag();
+                playerRef.rb.linearDamping = 10.0f;
+            }
+        }
+        StartCoroutine(DestroyGameObject(_waitTime));
+        Destroy(this.gameObject);
+    }
+    public void OnCollisionExit(Collision player) {
+        PlayerReferences playerRef = player.gameObject.GetComponent<PlayerReferences>();
+        playerRef.rb.angularDamping = 0.0f;
+        playerRef.rb.linearDamping = 0.0f;
+    }
+    public IEnumerator DestroyGameObject(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(this.gameObject);
     }
 }
