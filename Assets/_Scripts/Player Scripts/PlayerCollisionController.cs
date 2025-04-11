65f8bc525a8c384455fef818640156969698b337
Einar <einarsterten@gmail.com>
2025-04-11 11:10:49 +0200

    Spring cleaning part 2

--- Assets/_Scripts/Player Scripts/PlayerCollisionController.cs
+++ Assets/_Scripts/Player Scripts/PlayerCollisionController.cs
@@ -4,16 +4,16 @@ public class PlayerCollisionController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private PlayerPowerController powerController;
-    [SerializeField] private PlayerReferences ModelController;
+    [SerializeField] private PlayerReferences playerReference;
 
     private void Start()
     {
-        powerController = ModelController.PlayerPowerController;
+        powerController = playerReference.PlayerPowerController;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         powerController.PlayerCollision(collision);
-        collision.gameObject.GetComponent<PlayerReferences>().PlayerStats.PlayerKills(1);
+        playerReference.PlayerStats.PlayerKills(1);
     }
 }
