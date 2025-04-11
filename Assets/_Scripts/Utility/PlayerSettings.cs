b7be7db5accb6adda14942c473712ec837798183
iHateThisName <ivabr@stud.ntnu.no>
2025-04-11 16:26:13 +0200

    Add volume settings to PlayerSettings

--- Assets/_Scripts/Utility/PlayerSettings.cs
+++ Assets/_Scripts/Utility/PlayerSettings.cs
@@ -2,4 +2,23 @@ public static class PlayerSettings {
 
     public static bool IsLandscape = true;
     public static string SelectedLevel = "Level 01, LasVegas";
+
+    // Sounds
+    public static float MasterVolume = 1f;
+    private static float _worldVolume = 0.3f;
+    private static float _sfxVolume = 0.5f;
+    public static float WorldVolume {
+        get => ApplyMasterVolume(_worldVolume);
+        set => _worldVolume = value;
+    }
+    public static float SFXVolume {
+        get => ApplyMasterVolume(_sfxVolume);
+        set => _sfxVolume = value;
+    }
+
+    private static float ApplyMasterVolume(float rawVolume) {
+        return MasterVolume * rawVolume;
+    }
+
+
 }
