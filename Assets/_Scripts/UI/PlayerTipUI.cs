11018e6fb3e0ac26c20a321669be073f6612343e
PythonXYZ <hilmirsg@gmail.com>
2025-04-25 13:26:24 +0200

    Player Tip UI Fixed
    Now it's finished (hopefully)

--- Assets/_Scripts/UI/PlayerTipUI.cs
+++ Assets/_Scripts/UI/PlayerTipUI.cs
@@ -7,33 +7,33 @@ public class PlayerTipUI : MonoBehaviour
     [SerializeField] private CanvasGroup _uiTipPrefab;
     [SerializeField] private PlayerReferences _playerReferences;
 
+    private bool _fadeIn = false;
+    [SerializeField] private float _timeToFadeIn;
+
     private void Start() {
         _uiTipPrefab.alpha = 0f;
     }
-    /*private void Update() {
-        Invoke("FadeInUI", 5f);
-    }*/
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Plunger"))
         {
+            _fadeIn = true;
             print("player is colliding with plunger");
-            Invoke("FadeInUI", 5f);
+            // Invoke("FadeInUI", 5f);
+            StartCoroutine(FadeInUICoroutine());
         }
-        else if (!_playerReferences.gameObject.CompareTag("Plunger"))
+        else
         {
             print("player is NOT colliding with plunger");
             _uiTipPrefab.alpha = 0f;
         }
     }
-    /*private void OnTriggerExit(Collider other) {
-        if (other.gameObject.CompareTag("Plunger"))
-        {
-            _uiTipPrefab.alpha = 0f;
-        }
-    }*/
 
-    private void FadeInUI() {
-        _uiTipPrefab.alpha += Time.deltaTime;
+    private IEnumerator FadeInUICoroutine() {
+        yield return new WaitForSeconds(5f);
+        if (_fadeIn == true)
+        {
+            _uiTipPrefab.alpha += _timeToFadeIn * Time.deltaTime;
+        }
     }
 }
