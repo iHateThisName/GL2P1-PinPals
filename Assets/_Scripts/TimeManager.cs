83853aae29ddb12647ebcaeb12cdb249c39a651d
Einar <einarsterten@gmail.com>
2025-04-30 13:49:56 +0200

    Changing timer script, counddown sfx, changing timesupCanvas. vgas lvl

--- Assets/_Scripts/TimeManager.cs
+++ Assets/_Scripts/TimeManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+//Einar
 
 public class TimeManager : Singleton<TimeManager> {
     //A bunch of other variables
@@ -9,11 +10,13 @@ public class TimeManager : Singleton<TimeManager> {
     float currentTime;
 
     [SerializeField] public int counter = 120;
-    public TMP_Text currentTimeText;
+    //public TMP_Text currentTimeText;
     public GameObject restartMenu;
 
-    [SerializeField] private AudioClip gameOverSFX;
+    //[SerializeField] private AudioClip gameOverSFX;
+    [SerializeField] private AudioClip countDownSFX;
 
+    private bool hasPlayedCountdown = false;
     public string TimerText = "00:00";
 
     // Start is called before the first frame update
@@ -23,32 +26,39 @@ public class TimeManager : Singleton<TimeManager> {
         //stopWatchActive = true;
 
         currentTime = counter;
+        hasPlayedCountdown = false;
 
-
-    }
+}
 
     public void FixedUpdate() {
-        if (currentTimeText == null) return;
+        //if (currentTimeText == null) return;
         if (stopWatchActive == true) {
+
+            if (!hasPlayedCountdown && currentTime <= 10f && currentTime > 0f)
+            {
+                
+                SoundEffectManager.Instance.PlaySoundFXClip(countDownSFX, transform, 1f);
+                hasPlayedCountdown = true;
+            }
             if (currentTime > 0) {
                 currentTime -= Time.deltaTime;
 
             } else {
-                SoundEffectManager.Instance.PlaySoundFXClip(gameOverSFX, transform, 1f);
+                //SoundEffectManager.Instance.PlaySoundFXClip(gameOverSFX, transform, 1f);
                 stopWatchActive = false;
                 restartMenu.SetActive(true);
                 Time.timeScale = 0;
 
-                StartCoroutine(DelayLoade());
+                StartCoroutine(DelayLoad());
             }
         }
 
         TimeSpan time = TimeSpan.FromSeconds(currentTime);
         this.TimerText = "Time left: " + time.ToString(@"mm\:ss\.ff");
-        currentTimeText.text = TimerText;
+        //currentTimeText.text = TimerText;
     }
 
-    private IEnumerator DelayLoade() {
+    private IEnumerator DelayLoad() {
         yield return new WaitForSecondsRealtime(1);
         GameManager.Instance.ScoreScene();
     }
