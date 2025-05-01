426b7d9f7837108807409ca0b07f3ecc947bdd87
Einar <einarsterten@gmail.com>
2025-05-01 12:08:58 +0200

    Importing vfx package

---
+++ Assets/_Scripts/VFX/FreezeEffectScrpit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class FreezeEffectScrpit : MonoBehaviour
+{
+    [SerializeField] MeshRenderer sphereSkin;
+
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
