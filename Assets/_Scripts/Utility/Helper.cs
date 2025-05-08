12e973eba6a39a54dd0ca03c15c8ef2ea1b5fd09
Inga Sigurros Thorisdottir <ingasigurros4@gmail.com>
2025-05-08 12:40:08 +0200

    Changed the name of skeleton to 6 feet under and added the ground

--- Assets/_Scripts/Utility/Helper.cs
+++ Assets/_Scripts/Utility/Helper.cs
@@ -7,7 +7,7 @@ public static class Helper {
     public static List<Material> UsedPinballMaterials = new List<Material>();
     public static Dictionary<EnumPlayerTag, GameObject> Players = new Dictionary<EnumPlayerTag, GameObject>();
 
-    public const string level1 = "Level 01, LasVegas", level2 = "Level 02, PipelineMayhem", level3 = "Level 03, Skeleton", level4 = "Level 04, Wide-West",
+    public const string level1 = "Level 01, LasVegas", level2 = "Level 02, PipelineMayhem", level3 = "Level 03, 6FeetUnder", level4 = "Level 04, Wide-West",
         level5 = "Level 05, HauntedPinMachine", level6 = "Level 06, The Ring", levelSelector = "LevelSelector", endGame = "EndGame", mainMenu = "MainMenu", lobby = "Lobby";
 
     public static class LobbyButtonCoords {
