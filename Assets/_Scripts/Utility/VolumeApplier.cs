6665e275c6ee89d448735b12889c959f99887b6d
iHateThisName <ivarravi0@gmail.com>
2025-05-15 11:10:00 +0200

    Allow to live update music.

--- Assets/_Scripts/Utility/VolumeApplier.cs
+++ Assets/_Scripts/Utility/VolumeApplier.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 
 public class VolumeApplier : MonoBehaviour {
-    [SerializeField] private bool isSFX = true;
+    [SerializeField] private bool isSFX = true, liveUpdate = false;
     private AudioSource audioSource;
 
     void Start() {
         this.audioSource = GetComponent<AudioSource>();
 
+        SetVolume();
+    }
+
+    private void Update() {
+        if (liveUpdate) {
+            SetVolume();
+        }
+    }
+
+    public void SetVolume() {
         if (isSFX) {
             this.audioSource.volume = SoundManager.Instance.SFXVolume;
         } else {
             this.audioSource.volume = SoundManager.Instance.MusicVolume;
         }
     }
-
 }
