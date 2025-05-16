c8bf94ea0d2b5ee5676f142d0c8d8db3e9ca904e
PythonXYZ <hilmirsg@gmail.com>
2025-05-16 10:49:24 +0200

    Finished fixing the honey block powerup after almost 5 HOURS BROOO

--- Assets/_Scripts/PowerUps/HoneyBlock.cs
+++ Assets/_Scripts/PowerUps/HoneyBlock.cs
@@ -1,47 +1,71 @@
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
 public class HoneyBlock : MonoBehaviour
 {
-    private GameObject _honeyOwner;
-    private float _waitTime = 5f;
-    /*public void AssignOwner(GameObject owner) {
-        this._honeyOwner = owner;
-    }*/
+    [SerializeField] private Vector3 _initialVelocity;
+    private float _waitTime = 1f;
+    private int _index = 0;
+    private List<PlayerReferences> _players = new (); // Have the plater remember the initialvelocity in player references or have a reference to the player. or I can address the lists adjasently // if an object has alrady been detected, don't add a player to the lisyt already.
+    private List<Vector3> _velocity = new();
+    private List<EnumPlayerTag> _tags = new ();
     private void Start() {
         this.gameObject.SetActive(true);
     }
-    public void OnCollisionStay(Collision player) {
+    public void OnTriggerEnter(Collider player) {
+        SlowPlayer(player);
+    }
+    public void OnTriggerStay(Collider other) {
+        SlowPlayer(other);
+    }
 
+    public void SlowPlayer(Collider player) {
+        PlayerReferences playerReferences = player.GetComponent<PlayerReferences>();
+        if (_tags.Contains(playerReferences.GetPlayerTag()))
+            return;
+        _tags.Add(playerReferences.GetPlayerTag());
+        _velocity.Add(player.GetComponent<Rigidbody>().linearVelocity);
+        _players.Add(playerReferences);
+        Debug.Log("Honey On" + player.gameObject.name);
         if (player.gameObject.name.Contains("Clone"))
         {
-            //this.gameObject.GetComponent<PhysicsMaterial>().staticFriction = 10f;
-            this.gameObject.GetComponent<PhysicsMaterial>().dynamicFriction = 10f;
-            player.gameObject.GetComponent<Rigidbody>().linearDamping = 10.0f;
-            player.gameObject.GetComponent<Rigidbody>().angularDamping = 100f;
+            //player.gameObject.GetComponent<Rigidbody>().linearDamping = 100.0f;
+            //player.gameObject.GetComponent<Rigidbody>().angularDamping = 100f;
+            player.gameObject.GetComponent<PhysicsMaterial>().dynamicFriction = 100f;
         }
         // Detects the players and de-accelerates their speed.
 
         if (player.gameObject.tag.StartsWith("Player"))
         {
-            PlayerReferences playerRef = player.gameObject.GetComponent<PlayerReferences>();
-            EnumPlayerTag tag = playerRef.GetPlayerTag();
-            this.gameObject.GetComponent<PhysicsMaterial>().dynamicFriction = 100f;
-            playerRef.rb.linearDamping = 10;
-            playerRef.rb.angularDamping = 100;
+            Debug.Log("Honey On" + player.gameObject.name);
+            EnumPlayerTag tag = playerReferences.GetPlayerTag();
+            //player.gameObject.GetComponent<Collider>().material.dynamicFriction = 100f;
+            playerReferences.rb.linearDamping = 25f;
+            _initialVelocity = playerReferences.rb.linearVelocity;
+            playerReferences.rb.linearVelocity = _initialVelocity * .5f;
+            StartCoroutine(DestroyGameObject(_waitTime));
         }
-
-        StartCoroutine(DestroyGameObject(_waitTime));
-        Destroy(this.gameObject);
     }
-    public void OnCollisionExit(Collision player) {
+    public void OnTriggerExit(Collider player) {
+        Debug.Log("Honey Off" + player.gameObject.name);
         PlayerReferences playerRef = player.gameObject.GetComponent<PlayerReferences>();
-        playerRef.rb.angularDamping = 0.0f;
-        playerRef.rb.linearDamping = 0.0f;
-        this.gameObject.GetComponent<PhysicsMaterial>().dynamicFriction = 1f;
+        playerRef.rb.linearDamping = 0;
     }
     public IEnumerator DestroyGameObject(float waitTime) {
-        yield return new WaitForSeconds(waitTime);
-        Destroy(this.gameObject);
+        yield return new WaitForSecondsRealtime(waitTime);
+        ResetPlayerSpeed();
+        this.gameObject.SetActive(false);
+    }
+
+    public void ResetPlayerSpeed() {
+        foreach (PlayerReferences player in _players) {
+            foreach (Vector3 velocity in _velocity) {
+            _index = _velocity.IndexOf(velocity);
+            }
+            player.rb.linearDamping = 0;
+        }
+        _players.Clear();
+        _velocity.Clear();
     }
 }
