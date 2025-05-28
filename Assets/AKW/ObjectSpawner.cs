67f3e91af019b23f6f8700be8ae5fc72908c76fc
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2025-05-28 12:42:28 +0200

    Bomb Dropper skript

--- Assets/AKW/ObjectSpawner.cs
+++ Assets/AKW/ObjectSpawner.cs
@@ -5,9 +5,27 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject ObjectToSpawn;
     public float minSpawnTime = 5f;
     public float maxSpawnTime = 10f;
+    public float objectLifetime = 10f;
+
+    private float nextSpawnTime;
+
+    void Start()
+    {
+        SetNextSpawnTime();
+    }
 
     void Update()
     {
-        
+        if (Time.time >= nextSpawnTime)
+        {
+            GameObject spawnedObject = Instantiate(ObjectToSpawn, transform.position, Quaternion.identity);
+            Destroy(spawnedObject, objectLifetime);
+            SetNextSpawnTime();
+        }
+    }
+
+    void SetNextSpawnTime()
+    {
+        nextSpawnTime = Time.time + Random.Range(minSpawnTime, maxSpawnTime);
     }
 }
