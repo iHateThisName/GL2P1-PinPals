674ca4a73abefa65561fa94bbdd07302fd7dc66c
iHateThisName <ivarravi0@gmail.com>
2025-06-02 15:23:50 +0200

    WideWest, plunger pistol

--- Assets/Scenes/Rares/Scripts/WideWestRevolverPlunger.cs
+++ Assets/Scenes/Rares/Scripts/WideWestRevolverPlunger.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class WideWestRevolverPlunger : MonoBehaviour
-{
+public class WideWestRevolverPlunger : MonoBehaviour {
     [Header("Rotation Settings")]
     public Vector3 rotationAmount = new Vector3(0, 0, 45); // Rotation applied while holding space
     public float rotationSpeed = 5f; // Speed of rotation
@@ -16,35 +15,28 @@ public class WideWestRevolverPlunger : MonoBehaviour
     private bool isHolding = false;
     private bool wasHolding = false;
 
-    void Start()
-    {
+    [SerializeField] private EnumPlayerTag playerTag = EnumPlayerTag.Player01;
+
+    void Start() {
         initialRotation = transform.rotation;
         targetRotation = initialRotation * Quaternion.Euler(rotationAmount);
     }
 
-    void Update()
-    {
-        isHolding = Input.GetKey(KeyCode.Space);
-        if (isHolding && !wasHolding)
-        {
-            if (pressClip != null && audioSource != null)
-            {
+    void Update() {
+        isHolding = GameManager.Instance.IsPlayerHoldingInteraction(this.playerTag);
+        if (isHolding && !wasHolding) {
+            if (pressClip != null && audioSource != null) {
                 audioSource.PlayOneShot(pressClip);
             }
         }
-        if (!isHolding && wasHolding)
-        {
-            if (releaseClip != null && audioSource != null)
-            {
+        if (!isHolding && wasHolding) {
+            if (releaseClip != null && audioSource != null) {
                 audioSource.PlayOneShot(releaseClip);
             }
         }
-        if (isHolding)
-        {
+        if (isHolding) {
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-        }
-        else
-        {
+        } else {
             transform.rotation = Quaternion.Lerp(transform.rotation, initialRotation, Time.deltaTime * rotationSpeed);
         }
         wasHolding = isHolding;
